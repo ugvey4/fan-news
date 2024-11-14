ef8a4e475671bd5e0437c7e8adc1f749c6791982
ugvey4 <141747638+ugvey4@users.noreply.github.com>
2024-11-14 18:07:35 +0300

    Update parsing.py

--- src/parsing.py
+++ src/parsing.py
@@ -4,8 +4,14 @@ import requests
 from bs4 import BeautifulSoup as Bs4
 from tkinter import *
 from ui import *
+import dbFan as db
 
-def launch_parser(text_widget):
+def launch_parser(text_widget, username):
+    team_names = db.get_teams_by_user(username)
+    if not team_names:
+        text_widget.insert(END, "Error: Team not found for the user.\n")
+        return
+
     headers = {
         'accept': '*/*',
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)\
@@ -27,20 +33,21 @@ def launch_parser(text_widget):
                 title = div.find('a').text
                 link = 'https://championat.com/' + str(div.find('a')['href'])
 
-                # Добавляем заголовок как гиперссылку
-                results += f"Дата: {date_today}, Время: {time}, "
-                results += f"{title}\n\n"
+                for team_name in team_names:
+                    if team_name[0].lower() in title.lower():
+                        results += f"Дата: {date_today}, Время: {time}, "
+                        results += f"{title}\n\n"
 
-                # Добавляем тег для гиперссылки
-                text_widget.insert(END, f"{title}\n", 'link')
-                text_widget.insert(END, f"Дата: {date_today}, Время: {time}\n\n")
+                        text_widget.insert(END, f"{title}\n", 'link')
+                        text_widget.insert(END, f"Дата: {date_today}, Время: {time}\n\n")
+                        text_widget.tag_bind('link', '<Button-1>', lambda e, url=link: open_link(url))
 
-                # Добавляем ссылку
-                text_widget.tag_bind('link', '<Button-1>', lambda e, url=link: open_link(url))
-
-            text_widget.insert(END, results)
+            if not results:
+                text_widget.insert(END, "Пока нет актуальных новостей.\n")
+            else:
+                text_widget.insert(END, results)
         else:
-            text_widget.insert(END, "Error: Unable to fetch the webpage.\n")
+            text_widget.insert(END, "Error\n")
 
     except Exception as e:
         text_widget.insert(END, f"An error occurred: {e}\n")
@@ -50,17 +57,18 @@ def open_link(url):
     import webbrowser
     webbrowser.open(url)
 
-def main():
+def main(username):
     window()
 
-    text_widget = Text(root, wrap='word', height=20, width=80, bg= '#2f2f2f', fg= 'white',
-    font= ('Helvetica', 12 ))
+    text_widget = Text(root, wrap='word', height=20, width=80, bg='#2f2f2f', fg='white',
+                       font=('Helvetica', 12))
     text_widget.pack(expand=True, fill='both')
 
-    fetch_button = Button(root, text="Fetch News", command=lambda: launch_parser(text_widget))
+    fetch_button = Button(root, text="Fetch News", command=lambda: launch_parser(text_widget, username))
     fetch_button.pack(pady=10)
 
     root.mainloop()
 
 if __name__ == "__main__":
-    main()
+    user = "example_username"
+    main(user)
